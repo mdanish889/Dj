40da8e1fa71d9c2789d24c6693c58aa4c1de516f
MDanish889 <muhdanish889@gmail.com>
2020-04-13 23:57:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dj/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.dj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText NumberOneET, NumberTwoET;
+    TextView result;
+    int var1, var2, res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        javaconnectionwithxml();
+    }
+
+
+    private void javaconnectionwithxml() {
+        NumberOneET = findViewById(R.id.numberOneET);
+        NumberTwoET = findViewById(R.id.numberTwoET);
+        result = findViewById(R.id.result);
+
+    }
+
+    public void add1(View objView) {
+        if (!NumberOneET.getText().toString().isEmpty() && (!NumberTwoET.getText().toString().isEmpty())) {
+            var1 = Integer.parseInt(NumberOneET.getText().toString());
+            var2 = Integer.parseInt(NumberTwoET.getText().toString());
+            res = var1 + var2;
+        }
+        else if(NumberOneET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_one, Toast.LENGTH_SHORT).show();
+        }
+        else if(NumberTwoET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_two, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    public void mul1(View objView) {
+        if (!NumberOneET.getText().toString().isEmpty() && (!NumberTwoET.getText().toString().isEmpty())) {
+            var1 = Integer.parseInt(NumberOneET.getText().toString());
+            var2 = Integer.parseInt(NumberTwoET.getText().toString());
+            res = var1 *var2;
+        }
+        else if(NumberOneET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_one, Toast.LENGTH_SHORT).show();
+        }
+        else if(NumberTwoET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_two, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void div1(View objView) {
+        if (!NumberOneET.getText().toString().isEmpty() && (!NumberTwoET.getText().toString().isEmpty())) {
+            var1 = Integer.parseInt(NumberOneET.getText().toString());
+            var2 = Integer.parseInt(NumberTwoET.getText().toString());
+            res = var1 + var2;
+        }
+        else if(NumberOneET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_one, Toast.LENGTH_SHORT).show();
+        }
+        else if(NumberTwoET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_two, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    public void sub1(View objView) {
+        if (!NumberOneET.getText().toString().isEmpty() && (!NumberTwoET.getText().toString().isEmpty())) {
+            var1 = Integer.parseInt(NumberOneET.getText().toString());
+            var2 = Integer.parseInt(NumberTwoET.getText().toString());
+            res = var1 - var2;
+        }
+        else if(NumberOneET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_one, Toast.LENGTH_SHORT).show();
+        }
+        else if(NumberTwoET.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, R.string.plz_enter_value_two, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+}
